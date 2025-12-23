55a5fb845e223a369afa9925ce5a7ef2c0157b7f
Anubhav Mandarwal <socialengineerhundred@gmail.com>
2025-12-23 14:24:22 +0530

    Adding DVC

--- github-actions-mlops/train.py
+++ github-actions-mlops/train.py
@@ -6,6 +6,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, classification_report
+from sklearn.preprocessing import LabelEncoder
 import pickle
 import json
 import os
@@ -13,6 +14,7 @@ import os
 iris = load_iris()
 X = iris.data
 y = iris.target
+print(f"Loaded {len(X)} samples from sklearn dataset")
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
